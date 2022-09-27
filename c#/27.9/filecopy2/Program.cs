8b174872b5b0d05fe15695c705e0fe5b07f06c2b
AkshayalakshmiKaliyaperumal <113017040+AkshayalakshmiKaliyaperumal@users.noreply.github.com>
2022-09-27 22:45:20 +0530

    adding today's assignment

---
+++ c#/27.9/filecopy2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace directoryexc
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                try
+                {
+                    string[] s = Directory.GetFiles("c:\\Test");
+                    FileStream fs = new FileStream(s[0], FileMode.Open);
+                    StreamReader sr = new StreamReader(fs);
+                    Console.WriteLine(sr.ReadToEnd());
+                }
+                catch (UnauthorizedAccessException e) { Console.WriteLine(e.Message); }
+                catch (DirectoryNotFoundException e) { Console.WriteLine(e.Message); }
+                catch (FileNotFoundException e) { Console.WriteLine(e.Message); }
+                
+            }
+        }
+    }
+
+   
