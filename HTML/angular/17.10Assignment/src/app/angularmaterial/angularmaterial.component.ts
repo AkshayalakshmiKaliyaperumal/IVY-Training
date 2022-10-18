d9af414b0ebdf58e072197eb893dee4daa72fd48
AkshayalakshmiKaliyaperumal <113017040+AkshayalakshmiKaliyaperumal@users.noreply.github.com>
2022-10-18 15:53:25 +0530

    uploaded

---
+++ HTML/angular/17.10Assignment/src/app/angularmaterial/angularmaterial.component.ts
@@ -0,0 +1,9 @@
+import { formatCurrency } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-angularmaterial',
+  templateUrl: './angularmaterial.component.html',
+  styleUrls: ['./angularmaterial.component.css']
+})
+export class AngularmaterialComponent{}
